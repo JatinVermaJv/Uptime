2146f7359eaab58d7117e1046158aeff847cf461
JatinVermaJv <jatinverma5868@gmail.com>
2025-04-07 16:58:29 +0530

    Importing the authmiddleware in the auth routes

--- server/src/routes/auth.js
+++ server/src/routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const { JWT_SECRET } = require('../middleware/auth');
+const { JWT_SECRET, authMiddleware } = require('../middleware/auth');
 const prisma = require('../db');
 
 const router = express.Router();
